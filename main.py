5387590820c9adecfed1c5493d9f57b7b2966d60
doc_mhvr <mahavir37chandaliya@gmail.com>
2020-07-01 19:24:44 +0530

    Defined initial classes and methods

--- main.py
+++ main.py
@@ -2,3 +2,24 @@ import pandas
 import numpy
 import tkinter
 import sklearn
+class learning():
+    def __init__(self):
+        pass
+    def linear_reg(self):
+        pass
+    def svm(self):
+        pass
+    def k_means(self):
+        pass
+    def k_nearest(self):
+        pass
+
+def load_data():
+    pass
+
+def main():
+    tkinter.Tk().inti()
+
+
+
+if __name__ == "__main__" : main()
