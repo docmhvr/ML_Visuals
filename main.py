e89e54a62da22c98d9fab811fb25e5b0be0f92b4
doc_mhvr <mahavir37chandaliya@gmail.com>
2020-07-01 23:35:20 +0530

    Completed most GUI part

--- main.py
+++ main.py
@@ -1,31 +1,47 @@
 import pandas as pd
-import numpy
+import numpy as np
 import tkinter
-import tkinter.ttk as ttk
+from tkinter import ttk
 import sklearn
-from tkinter import scrolledtext
 from tkinter import messagebox
 from tkinter.filedialog import *
 
 
 class learning():
-    def __init__(self):
-        pass
+    def __init__(self,train):
+        self.train = train
+        self.model = None
+        self.result = None
+        self.x_train,self.x_test,self.y_train,self.y_test = sklearn.model_selection.train_test_split(x_data,y_data,test_size=0.2)
 
+    # TODO
     def data_checker(self):
         pass
 
+    # TODO
     def linear_reg(self):
-        pass
+        self.model = sklearn.linear_model.LinearRegression()
+        self.model.fit(self.x_train,self.y_train)
+        self.result = self.model.score(self.x_test,self.y_test)
 
+    # TODO
     def svm_cls(self):
-        pass
+        self.model = sklearn.svm.SVM()
+        self.model.fit(self.x_train, self.y_train)
+        # self.result=
 
+    # TODO
     def k_means_ctn(self):
-        pass
+        self.model = sklearn.cluster.KMeans()
+        self.model.fit(self.x_train, self.y_train)
+        # self.result =
 
+    # TODO
     def k_nearest_nbr(self):
-        pass
+        self.model = sklearn.neighbors.KNeighborsClassifier()
+        self.model.fit(self.x_train, self.y_train)
+        # self.result =
+
     """
     further dev
     def logistic_reg(self):
@@ -40,43 +56,60 @@ class learning():
         pass
         
     """
+def train_model():
+    print(algochooser.get())
 
 def load_data():
     file = askopenfile(mode='r', filetypes=[('csv files', '*.csv',)])
     if file is not None:
         data = pd.read_csv(file)
-        print(data.head())
     else:
         messagebox.showinfo("Note", "You haven't loaded clean data")
 
+# TODO
 def main():
     #defining tkinter window
     win = tkinter.Tk()
     win.title("Machine Learning Visuals")
     # win.geometry("720x720")
     # win.attributes('-fullscreen', True)
+
+    # Styling
+    s = ttk.Style(win)
+    s.theme_use("vista")
+    s.configure("TButton",font =('Times',10,'bold'), foreground="black", background='black')
+    s.configure("TLabel",font =('Times',10,'bold'), foreground="white", background="black")
+    s.configure("TFrame", foreground="white", background="black")
+    s.configure("TCombobox", font=('Times', 10, 'bold'), foreground="black", background="black")
+    # print(s.theme_names())
+
     # defining top,mid and end frames for loading data, Applying ML Algos and showing results
-    top_frame = tkinter.Frame(win, bg="red")
+    top_frame = ttk.Frame(win)
     top_frame.grid(row=0, sticky="ew")
-    mid_frame = tkinter.Frame(win, bg="green")
+    mid_frame = ttk.Frame(win)
     mid_frame.grid(row=1, sticky="ew")
-    end_frame = tkinter.Frame(win, bg="blue")
+    end_frame = ttk.Frame(win)
     end_frame.grid(row=2, sticky="ew")
 
-    load = tkinter.Label(top_frame, text="Load Your Clean Dataset")
+    load = ttk.Label(top_frame, text="Load Your Clean Dataset")
     load.pack(side="left", padx=5, pady=5)
-    loadbtn = tkinter.Button(top_frame, text="Load", command=load_data)
+    loadbtn = ttk.Button(top_frame, text="Load", command=load_data)
     loadbtn.pack(side="left", padx=5, pady=5)
 
-    algolabel = tkinter.Label(mid_frame, text="Select Model You want to train")
+    algolabel = ttk.Label(mid_frame, text="Select Model You want to train")
     algolabel.pack(side="left", padx=5, pady=5)
     algochooser = ttk.Combobox(mid_frame)
     algochooser['values'] = ("linear_reg", "svm_cls", "k_means_ctn", "k_nearest_nbr")
     algochooser.current(0)
     algochooser.pack(side="left", padx=5, pady=5)
+    trainbtn = ttk.Button(mid_frame, text="Train", command=train_model)
+    trainbtn.pack(side="right", padx=5, pady=5)
+
+    resultlabel = ttk.Label(end_frame, text="The accuracy of the model is:")
+    resultlabel.pack(side="left", padx=5, pady=5)
+    result = ttk.Label(end_frame, text="test.result")
+    result.pack(side="left", padx=5, pady=5)
 
     win.mainloop()
-
-
 
 if __name__ == "__main__" : main()
